ceadf7fa7d3f84ec6c4658ed3562a1a8bf15b401
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-18 22:16:35 +0000

    feat: implement email business logic

--- domain/core/src/pt/med/wallet/domain/core/valueobjects/Email.scala
+++ domain/core/src/pt/med/wallet/domain/core/valueobjects/Email.scala
@@ -1,5 +1,20 @@
 package pt.med.wallet.domain.core.valueobjects
 
 import pt.med.wallet.ValueObject
+import pt.med.wallet.domain.core.exceptions.EmailException
 
-case class Email(email: String) extends ValueObject[String]
+case class Email(email: String) extends ValueObject[String](email)
+
+object Email {
+
+  def apply(email: String): Email = {
+    if(isValidEmail(email)) new Email(email) else throw EmailException(s"This email: $email is not valid")
+  }
+
+  def apply(name: String)(domain: String): Email = {
+    val email: String = s"$name@$domain"
+    Email(email)
+  }
+
+  private def isValidEmail(email: String): Boolean = email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
+}
