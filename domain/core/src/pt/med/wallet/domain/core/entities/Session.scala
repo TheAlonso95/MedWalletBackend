90f97819bc748c3df5fc2db2fadef778e9347924
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-21 21:58:59 +0000

    feat: implement Session business logic

--- domain/core/src/pt/med/wallet/domain/core/entities/Session.scala
+++ domain/core/src/pt/med/wallet/domain/core/entities/Session.scala
@@ -1,8 +1,22 @@
 package pt.med.wallet.domain.core.entities
 
 import pt.med.wallet.Entity
-import pt.med.wallet.domain.core.valueobjects.SessionId
+import pt.med.wallet.domain.core.valueobjects.{Email, SessionId}
 
 import java.time.LocalDateTime
+import java.util.{Base64, UUID}
 
-case class Session(sessionId: SessionId, token: String, expirationDate: LocalDateTime) extends Entity[Session](sessionId)
+case class Session(sessionId: SessionId, token: String, expirationDate: LocalDateTime)
+  extends Entity[Session](sessionId) {
+
+  def isValid: Boolean = expirationDate.isAfter(LocalDateTime.now())
+}
+
+object Session {
+  def generateSession(email: Email): Session = {
+    val id = UUID.randomUUID()
+    val token = Base64.getEncoder.encodeToString(s"$email-token-$id".getBytes)
+    val expirationAt = LocalDateTime.now().plusHours(24)
+    Session(new SessionId(id), token, expirationAt)
+  }
+}
