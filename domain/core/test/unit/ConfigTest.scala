86b9327e1f4bd774eadefb2082d60c685351da9d
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-23 15:22:21 +0000

    test: create unit tests for Config logic

---
+++ domain/core/test/unit/ConfigTest.scala
@@ -0,0 +1,38 @@
+package unit
+
+import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+import pt.med.wallet.domain.core.exceptions.ConfigException
+import pt.med.wallet.domain.core.{PasswordConf, SessionConf}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class ConfigTest extends AnyFlatSpec with should.Matchers {
+  "A session configuration" should "parse correctly the config values present on the test.conf" in {
+      val sessionConf = SessionConf.load("test.conf")
+      val expectedConfig = SessionConf(cookieName = "med_cookie", expirationTime = 24 hours, secureCookie = false)
+      sessionConf shouldBe expectedConfig
+    }
+
+    "A session configuration when failing to extract the config file" should "generate a session config with default values" in {
+      val sessionConf = SessionConf.load("non-existent.conf")
+      val expectedConfig = SessionConf()
+
+      sessionConf shouldBe expectedConfig
+    }
+
+  "A password configuration" should "parse correctly the config values present on the test.conf" in {
+    val passwordConf = PasswordConf.load("test.conf")
+    val expectedConfig = PasswordConf(minLength = 8, maxLength = 30, withUpperCase = true, secretKey = "secret_key_test_123")
+    passwordConf shouldBe expectedConfig
+  }
+
+  "A session password when failing to extract the config file" should "generate a session config with default values" in {
+    assertThrows[ConfigException]{
+      PasswordConf.load("failure.conf")
+    }
+
+  }
+}
