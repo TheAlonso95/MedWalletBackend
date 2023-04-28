ef9bdd2e066584ebf9cf61679748f326024bc300
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-28 22:28:49 +0000

    refactor: make account id a UUID

--- domain/application/src/pt/med/wallet/domain/application/dtos/AccountDTO.scala
+++ domain/application/src/pt/med/wallet/domain/application/dtos/AccountDTO.scala
@@ -4,7 +4,9 @@ import pt.med.wallet.domain.core.PasswordConf
 import pt.med.wallet.domain.core.entities.Account
 import pt.med.wallet.domain.core.valueobjects.{AccountId, Email, Password}
 
-case class AccountDTO(accountId: Long, email: String, password: String, session: Option[SessionDTO] = None) {
+import java.util.UUID
+
+case class AccountDTO(accountId: UUID, email: String, password: String, session: Option[SessionDTO] = None) {
   def toAccount(implicit config: PasswordConf): Account =
     Account(new AccountId(accountId), Email(email), Password(password), session.map(_.toSession))
 }
--- domain/core/src/pt/med/wallet/domain/core/valueobjects/AccountId.scala
+++ domain/core/src/pt/med/wallet/domain/core/valueobjects/AccountId.scala
@@ -2,4 +2,6 @@ package pt.med.wallet.domain.core.valueobjects
 
 import pt.med.wallet.Id
 
-class AccountId(id: Long) extends Id[Long](id)
+import java.util.UUID
+
+class AccountId(id: UUID) extends Id[UUID](id)
