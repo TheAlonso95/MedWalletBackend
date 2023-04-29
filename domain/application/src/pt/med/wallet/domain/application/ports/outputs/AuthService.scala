a0c99c8523ac10f1c6d8b7d517788be1647bca99
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-29 16:38:55 +0000

    refactor: have only 1 endpoint to deal with updates

--- domain/application/src/pt/med/wallet/domain/application/ports/outputs/AuthService.scala
+++ domain/application/src/pt/med/wallet/domain/application/ports/outputs/AuthService.scala
@@ -1,22 +1,20 @@
 package pt.med.wallet.domain.application.ports.outputs
 
-import pt.med.wallet.domain.application.http.requests.{AccountRequest, DeleteAccountRequest, LoginRequest, SuspendAccountRequest, UpdateAccountRequest}
-import pt.med.wallet.domain.application.http.responses.LoginResponse
+import pt.med.wallet.domain.application.http.requests._
+import pt.med.wallet.domain.application.http.responses._
 
 import scala.concurrent.Future
 
 trait AuthService {
   def signIn(request: AccountRequest): Future[Unit]
 
-  def updatePassword(request: UpdateAccountRequest): Future[Unit]
+  def updateAccount(request: UpdateAccountRequest, token: String): Future[Unit]
 
-  def updateEmail(request: UpdateAccountRequest): Future[Unit]
-
-  def deleteAccount(request: DeleteAccountRequest): Future[Unit]
-
-  def suspendAccount(request: SuspendAccountRequest, reasons: List[String]): Future[Unit]
+  def deleteAccount(request: DeleteAccountRequest, token: String): Future[Unit]
 
   def login(request: LoginRequest): Future[LoginResponse]
 
-  def logout(request: String): Future[Unit]
+  def validateSession(request: ValidateSessionRequest): Future[Boolean]
+
+  def logout(request: LogoutRequest): Future[Unit]
 }
