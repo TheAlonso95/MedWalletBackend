5e64724981f3a6a3e8c825dacf17fc52ba3907aa
birdofartemis <marcomedeiros_03@outlook.com>
2023-04-29 16:41:04 +0000

    feat: add aux methods to UpdateAccountRequest

--- domain/application/src/pt/med/wallet/domain/application/http/requests/UpdateAccountRequest.scala
+++ domain/application/src/pt/med/wallet/domain/application/http/requests/UpdateAccountRequest.scala
@@ -2,6 +2,8 @@ package pt.med.wallet.domain.application.http.requests
 
 import play.api.libs.json.{Json, OFormat}
 import pt.med.wallet.domain.application.utils.AuthFormatter
+import pt.med.wallet.domain.core.PasswordConf
+import pt.med.wallet.domain.core.valueobjects.{Email, Password}
 
 case class UpdateAccountRequest(
                                  email: String,
@@ -12,7 +14,11 @@ case class UpdateAccountRequest(
 
   def isPasswordChangeRequest: Boolean = newPassword.isDefined
 
-  def isEmailChangeRequest: Boolean = newPassword.isEmpty
+  def isEmailChangeRequest: Boolean = !isPasswordChangeRequest
+
+  def toDomainPassword(implicit config: PasswordConf): Password = Password(newPassword.get, isEncrypted = true)
+
+  def toDomainEmail: Email = Email(newEmail.get)
 
 }
 
